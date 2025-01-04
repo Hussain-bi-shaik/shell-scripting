16db33a41f0e1e60aa78e4c9678f51fb34025403
shabbu <shabanashaik745@gmail.com>
2025-01-04 15:23:47 +0530

    new conversation

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=ramesh
+PERSON2=suresh
+echo "$PERSON1 :: hello $PERSON2"
+echo "$PERSON2 :: hii $PERSON1"
+echo "$PERSON1 :: how are you $PERSON2"
+echo "$PERSON2 :: fine $PERSON1"
