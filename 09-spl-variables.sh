e7c31e9c9c23f5e332e424f9003d5ccfb2fd94fc
shabbu <shabanashaik745@gmail.com>
2025-01-05 13:59:38 +0530

    conversation has added

---
+++ 09-spl-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "all variables passsed : $@"
+echo "number of vairables : $#"
+echo "script name : $0"
+echo "present working directory : $PWD"
+echo "home directory of current user : $home"
+echo "which user id running the script : $USER"
+echo "proceess id of current script : $$"
+sleep $60
+echo "process id of last command in background is " $!"
