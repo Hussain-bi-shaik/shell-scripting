708d47f93e4a0856eab39267dd97a109d029b40a
shabbu <shabanashaik745@gmail.com>
2025-01-05 16:15:52 +0530

    conversation has added

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)  # id -u is to check the id of current user
 if [ $USERID -ne 0 ]
   then
         echo "ERROR: you must have root access to install"
+        exit 1
 fi
 
 dnf install mysql -y
