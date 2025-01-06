0944fd86d858262e089c18173cdb12e4d9cedfb3
shabbu <shabanashaik745@gmail.com>
2025-01-06 19:23:20 +0530

    conversation added

--- 14-logs.sh
+++ 14-logs.sh
@@ -9,7 +9,7 @@ N="\E[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%D-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log$"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
