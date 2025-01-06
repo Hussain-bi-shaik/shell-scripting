15114481562a283a0f5ff3d763e3bee99ed0df04
shabbu <shabanashaik745@gmail.com>
2025-01-06 19:13:04 +0530

    conversation added

--- 14-logs.sh
+++ 14-logs.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
+
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%y-%m-%d-%h-%m-%s)
-LOG_FILE_NAME="$LOGS_FOLDER|$LOGS_FILE-$TIMESTAMP.log$"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\E[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%D-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log$"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
         then    
-                echo -e "$2...$R failure"
+                echo -e "$2...$R failure $N"
                 exit 1
      else
-        echo -e "$2...$G success"
+        echo -e "$2...$G success $N"
 
      fi              
 }
 echo "script executed at : "$TIMESTAMP" &>>$LOG_FILE_NAME
+
 if [ $USERID -ne 0 ]
     then
          echo -e "$R...error :: you must have root access to execute the command"
          exit 1
  fi
+
  dnf list installed mysql &>>$LOG_FILE_NAME
+
  if [ $? -ne 0 ]  
     then
             dnf install mysql -y &>>$LOG_FILE_NAME
             VALIDATE $? "installing mysql"
     else    
-         echo -e "my sql is already...$Y installed"
+         echo -e "my sql is already...$Y installed $NUMBER"
  fi
 
  dnf list installed git &>>$LOG_FILE_NAME
+
  if [ $? -ne 0 ] 
     then
             install dnf git -y &>>$LOG_FILE_NAME
